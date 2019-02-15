ae005a1b686c89e9db2e6dd99a31dc968d729dde
Wolf-Guido Bolick <wolf-guido.bolick@gmx.de>
2019-02-15 16:16:52 +0100

    Tool cluster_sdf_to_trainfolds:      - removed old code snippets

--- ga/Tools/cluster_sdf_to_trainfolds.py
+++ ga/Tools/cluster_sdf_to_trainfolds.py
@@ -8,71 +8,6 @@ import itertools
 def split_to_folds(filename,label):
     class_molecules = dict()
     class_molecules_keyset = set()
-    class_molecules_keyset.add(0)
-    class_molecules_keyset.add(1)
-    class_molecules_keyset.add(2)
-    class_molecules_keyset.add(3)
-    class_molecules_keyset.add(4)
-
-
-
-    # sdm = Chem.SDMolSupplier(filename, sanitize = False, removeHs= False, strictParsing=False)
-    # for mol in sdm:
-    #     mol_label = mol.GetProp(label)
-    #     if not mol_label in class_molecules_keyset:
-    #         class_molecules_keyset.add(mol_label)
-    #         class_molecules[mol_label] = []
-    #     class_molecules[mol_label].append(mol)
-    # pass
-    # for k in class_molecules_keyset:
-    #     print("cluster {} has {} mols".format(k,len(class_molecules[k])))
-
-    train_filename_temp = "trainset_{}.sdf"
-    test_filename_temp = "testset_{}.sdf"
-    inner_folder = "trainset_{}"
-    for k in class_molecules_keyset: # outer loop
-        outer_train_filename = "outer_{}".format(train_filename_temp.format(k))
-        outer_test_filename = "outer_{}".format(test_filename_temp.format(k))
-        sdw_otest = Chem.SDWriter(outer_test_filename)
-        sdw_otrain = Chem.SDWriter(outer_train_filename)
-        print("writer outer testset {}".format(k))
-        # for m in class_molecules[k]:
-        #     pass # write outer testset
-        sdw_otest.close()
-        trainkeys = class_molecules_keyset.difference(set([k]))
-        for l in trainkeys: # outer loop trainsets
-            inf = inner_folder.format(l)
-            p = pathlib.Path(inf).is_dir()
-            if not p:
-                os.mkdir(inf)
-            inner_train_filename = "{}/inner_{}".format(inf,train_filename_temp.format(l))
-            inner_test_filename = "{}/inner_{}".format(inf,test_filename_temp.format(l))
-            sdw_itest = Chem.SDWriter(inner_test_filename)
-            sdw_itrain = Chem.SDWriter(inner_train_filename)
-            print("\twrite trainfile {} using {}".format(outer_train_filename,l))
-            pass # writer outer trainset
-            sdw_itest.close()
-
-            print("\twrite inner testset {}".format(l))
-            # for m in class_molecules[l]:
-            #     pass # write inner test
-
-            inner_trainkeys = trainkeys.difference(set([l]))
-            for m in inner_trainkeys:
-                print("\t\twrite inner_trainfile {} using {}".format(inner_train_filename,m))
-            sdw_itrain.close()
-        sdw_otrain.close()
-
-
-def split_to_folds2(filename,label):
-    class_molecules = dict()
-    class_molecules_keyset = set()
-    # class_molecules_keyset.add(0)
-    # class_molecules_keyset.add(1)
-    # class_molecules_keyset.add(2)
-    # class_molecules_keyset.add(3)
-    # class_molecules_keyset.add(4)
-    # class_molecules = {k:[] for k in class_molecules_keyset}
 
     sdm = Chem.SDMolSupplier(filename, sanitize = False, removeHs= False, strictParsing=False)
     for mol in sdm:
@@ -100,13 +35,7 @@ def split_to_folds2(filename,label):
         outer_test_filename = "outer_{}".format(test_filename_temp.format(outer_test))
         print(outer_test_filename)
         print(outer_train_filename)
-        # sdw_otest = Chem.SDWriter(outer_test_filename)
-        # sdw_otrain = Chem.SDWriter(outer_train_filename)
 
-        # test = list(class_molecules_keyset.difference(outer_train_ids))
-
-        # test = test[0]
-        # print("outer_test: ", test)
         sdw_otest = Chem.SDWriter(outer_test_filename)
         for m in class_molecules[outer_test]:
             sdw_otest.write(m)
@@ -142,13 +71,11 @@ def split_to_folds2(filename,label):
             for inner_tra_id in inner_train_ids:
                 for m in class_molecules[inner_tra_id]:
                     sdw_itrain.write(m)
-            # print("inner_test: ", inner_test)
 
             sdw_itrain.close()
 
 if __name__ == '__main__':
     label = sys.argv[1]
     filename = sys.argv[2]
-    # split_to_folds(filename,label)
-    split_to_folds2(filename, label)
+    split_to_folds(filename, label)
 
